8adaa34a197c4a76c902acaa8305c748dc33a58c
Agustín Diaz <agusjd92@gmail.com>
2020-12-22 23:42:27 -0300

    commit

--- src/Components/ItemListContainer/ItemList/ItemList.js
+++ src/Components/ItemListContainer/ItemList/ItemList.js
@@ -19,7 +19,7 @@ const ItemList = () =>{
         <>
             {item.map((item) => {
                 return(
-                    <div className="col-xl-4 mb-4 mt-4">
+                    <div key ={item.id} className="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-4 mb-4 mt-4 ">
                         <h2>{item.title}</h2>
                         <p>{item.brand}</p>
                         <p>{item.price}</p>
